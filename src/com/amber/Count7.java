431473f3e217c82e36a3da3637eef118796bc6ee
Amber Jones <amberlovescats14@gmail.com>
2019-11-25 11:40:35 -0600

    completed my sixth recursion problem today

---
+++ src/com/amber/Count7.java
@@ -0,0 +1,17 @@
+package com.amber;
+
+public class Count7 {
+    public int getNumberOfSevens(int n){
+        String numString = String.valueOf(n);
+        if(numString.length() == 1)
+            if(n == 7) return 1;
+            else return 0;
+        else {
+            int first = Integer.parseInt(String.valueOf(numString.charAt(0)));
+            String sub = numString.substring(1);
+            int subNumber = Integer.parseInt(sub);
+            if( first == 7) return 1 + getNumberOfSevens(subNumber);
+            else return getNumberOfSevens(subNumber);
+        }
+    }
+}
