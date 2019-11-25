a1da2754b8c7987d2ad908244791774a27907136
Amber Jones <amberlovescats14@gmail.com>
2019-11-25 10:51:26 -0600

    finished fourth recursion problem

--- src/com/amber/SumDigits.java
+++ src/com/amber/SumDigits.java
@@ -1,12 +1,12 @@
-//package com.amber;
-//
-//public class SumDigits {
-//    public int getSumOfDigits(int n){
-//        String numString = String.valueOf(n);
-//        int numLength = numString.length();
-//        if(numLength == 1) return n;
-//        int firstNumber = (int)numString.charAt(0);
-//        System.out.println(firstNumber);
-//        return firstNumber
-//    }
-//}
+package com.amber;
+
+public class SumDigits {
+    public int getSumOfDigits(int n){
+        String numString = String.valueOf(n);
+        int numLength = numString.length();
+        if(numLength == 1) return n;
+        int firstNumber = Integer.parseInt(numString.substring(0,1));
+        int sub = Integer.parseInt(numString.substring(1));
+        return firstNumber + getSumOfDigits(sub);
+    }
+}
