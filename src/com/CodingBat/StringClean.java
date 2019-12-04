2e9995bf5e20689832a1b39dbf248fca234465fd
Amber Jones <amberlovescats14@gmail.com>
2019-12-04 12:04:57 -0600

    I needed to correct something on StringClean

--- src/com/CodingBat/StringClean.java
+++ src/com/CodingBat/StringClean.java
@@ -1,11 +1,18 @@
 package com.CodingBat;
 
 public class StringClean {
-    public String getRecursiveString(String str){
-        if(str.length() < 1) return "";
+    public static void main(String[] args) {
+        System.out.println(getRecursiveString("abbbcdd"));
+    }
+
+
+    public static String getRecursiveString(String str){
+        System.out.println(str);
+        if(str.length() < 2) return str;
         String first = String.valueOf(str.charAt(0));
         String second = String.valueOf(str.charAt(1));
-        if(first.equals(second)) return first + getRecursiveString(str.substring(2));
+        if(first.equals(second)) return getRecursiveString(str.substring(1));
         else return first + getRecursiveString(str.substring(1));
+
     }
 }
